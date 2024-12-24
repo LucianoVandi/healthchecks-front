6e574b9e2746877c6aadfae18b96e327dbc5d38a
Luciano <vandi.luciano@gmail.com>
2024-12-24 23:55:51 +0100

    refactor: apply reverted files from bb67b1beb961bb36291ed2ed9f70a2228f3ceb59 - step 1.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
-const path = require("path");
-
 module.exports = {
   reactStrictMode: true,
   i18n: {
-    locales: ["en", "it"], // Aggiungi qui i locali supportati
-    defaultLocale: "en", // Imposta il locale predefinito
-    localeDetection: true, // Abilita il rilevamento automatico del locale
+    locales: ["en", "it"],
+    defaultLocale: "en",
+    localeDetection: true,
   },
 }
